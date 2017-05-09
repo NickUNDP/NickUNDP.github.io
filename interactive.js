c448f25a788b2f759bc69aab50668a63cb127fc2
DRIC-Programme <nicholas.webster@undp.org>
2017-05-09 15:58:02 +0700

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -1,11 +1,11 @@
 function changeClass(ID,oclass,otherclass){
 	if (document.getElementById(ID).className===oclass){
 		document.getElementById(ID).className=otherclass
-		ID.style.opacity="0.0"
+		document.getElementById(ID).style.opacity="0.0"
 	}
 	else{
 	document.getElementById(ID).className=oclass
-	ID.style.opacity="1"
+	document.getElementById(ID).style.opacity="1"
 	}
 }
 
